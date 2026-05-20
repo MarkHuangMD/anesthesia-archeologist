2fff45be933e99490b2fa517a49ba08c577a866b
Egg -- 醬油蛋 <Sonofsunofson@gmail.com>
2026-05-20 19:55:06 +0800

    Create ch54.js

---
+++ chapters/ch54.js
@@ -0,0 +1,14 @@
+window.ch54_data = [
+    {
+        meta: "110年 筆試第25題",
+        question: "下列何者不是減重手術麻醉照顧需要注意的事項？",
+        options: [
+            "(A) 麻醉中使用靜脈輸注 propofol 較 desflurane 氣體麻醉恢復更快",
+            "(B) 術中需注意呼吸道處置、擺位和麻醉藥物劑量調整",
+            "(C) 術前評估包括合併症、呼吸道狀況、心臟功能和肝功能等",
+            "(D) Propofol、rocuronium、sugammadex 和 remifentanil 劑量可根據理想體重 (ideal body weight) 調整"
+        ],
+        answer: "(A)",
+        hint: "肥胖病患因脂肪組織多，脂溶性高的 Propofol 長時間輸注易累積；反之低血/氣溶解度的 Desflurane 甦醒速度顯著較快。"
+    }
+];
